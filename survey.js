aaba4c4422e564f4ecfc09299b474bd9dce606aa
‘Aarman’ <aaron.beerman@gmail.com>
2016-04-10 16:03:17 -0700

    all features working

--- survey.js
+++ survey.js
@@ -1,11 +1,14 @@
+// Create object constructor for photos of products
+var total = 0;
 function photosObj (names, src) {
   this.names = names;
+  this.id = names;
   this.src = src;
-  this.voteCount = 0;
+  this.y = 0;
   };
 
 // Array for Objects and their properties
-var photosArray = new Array();
+var photosArray = new Array(); // new array with new objects pushed in
 photosArray.push(new photosObj ("bag", "bag.jpg"));
 photosArray.push(new photosObj ("banana", "banana.jpg"));
 photosArray.push(new photosObj ("scissors", "scissors.jpg"));
@@ -22,40 +25,75 @@ photosArray.push(new photosObj ("cthulhu", "cthulhu.jpg"));
 photosArray.push(new photosObj ("dragon", "dragon.jpg"));
 
 
-var firstImage = document.getElementById('pic1');
-var secImage = document.getElementById('pic2');
-var thirdImage = document.getElementById('pic3');
+var cycledImgs = new Array (); // creates empty new array
 
-function randomPhoto() {
-    return Math.floor(Math.random() * photosArray.length);
-  }
-
-function displayImages() {
-    var div = document.getElementById('pic1');
-    rnd = randomPhoto();
-    var getPixs = photosArray[rnd]
-    div.innerHTML += "<img src="+getPixs.src+">";
-    var div = document.getElementById('pic2');
-    rnd = randomPhoto();
-    var getPixs = photosArray[rnd]
-    div.innerHTML += "<img src="+getPixs.src+">";
-    var div = document.getElementById('pic3');
-    rnd = randomPhoto();
-    var getPixs = photosArray[rnd]
-    div.innerHTML += "<img src="+getPixs.src+">";
+function makeArrayCopy() {
+for (var i = 0; i < photosArray.length; i++) {
+  cycledImgs.push(photosArray[i]); // empty array is populated by cycling original photos array with index
+  } // this creates a copy of the main array without overwriting original array...
 }
 
-if (firstImage === secImage) {var div = document.getElementById('pic2');
-  rnd = randomPhoto();
-  var getPixs = photosArray[rnd]
-  div.innerHTML += "<img src="+getPixs.src+">";
+function randomPhoto() {
+  if (cycledImgs.length == 0) {makeArrayCopy();
+  }
+    var i = Math.floor(Math.random() * cycledImgs.length);
+    var photoToUse = cycledImgs.splice(i, 1)[0];// brackets to reference first item in array
+    return photoToUse; // splice to remove a photo that has been used
+  } // use availablePhotos for length so that it it taking from an array that is getting smaller
+  // with each loop.
+
+function displayImages() { // create function to show 3 images
+  var placeHolder = document.getElementById('photos'); // get html id to place images in
+  placeHolder.innerHTML = ""; // IMPORTANT; used to clear images
+  for (var index = 0; index < 3; index++) { // create for loop to run func 3 times for 3 images
+    rnd = randomPhoto(); // random num generator with easy access var name
+    var getPixs = photosArray[rnd] // random product images is selected and placed in this var
+    var imageElement =  document.createElement("img"); // var is linked to the create a new element in html for the image
+    imageElement.src = rnd.src; // new img element is equated to random object property /.jpg refer/
+    imageElement.id = rnd.names; // same as above but instead the name to be used for id
+    imageElement.addEventListener("click", registerClick);
+    placeHolder.appendChild(imageElement);
+  }
+}
+function registerClick() {
+  // console.log("Image clicked");
+  // console.log(event.target.src);
+  console.log("clicked"+event.target.id);
+  total++;
+  for (var index=0; index < photosArray.length; index++) {
+    if (photosArray[index].names == event.target.id) {
+      photosArray[index].y++;
+      break; // stop loop
+      }
+    }
+    if (photosArray[index].y++ > 15) {alert("You have selected this image 15 times... you must love this product!");}
+  displayImages();
+  if (total > 14) {
+    displayChart();}
 }
 
 displayImages();
-//
-//
-// firstImage.addEventListener('click', );
-// secImage.addEventListener('click', );
-// thirdImage.addEventListener('click', );
 
-//
+
+var chart = null;
+function displayChart() {
+
+chart = new CanvasJS.Chart("chartContainer", {
+
+          title: {text: "Clicks Per Photo"},
+          data: [//array of dataSeries
+            /*** Change type "column" to "bar", "area", "line" or "pie"***/
+                  {
+                   type: "column",
+                   dataPoints: photosArray
+                  }
+                ]
+         });
+
+        chart.render();
+      }
+
+function showChart() {
+  console.log("showing chart");
+  chart.render();
+}
